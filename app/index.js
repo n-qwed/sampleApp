2cb5070c7dd4ea64c83f99f660a9db5ab0f56ff7
naohiro.tateishi <qwed0710@gmail.com>
2018-06-22 18:08:13 +0900

    modify

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { store } from './store';
 import Ascreen from './containers/Ascreen';
 import Bscreen from './containers/Bscreen';
